5f92b16654cff56dfe2f026c71f7617ce48c841c
drewlarso <drewlarso1@gmail.com>
2024-09-30 22:00:09 -0600

    added zoom and origin to mandelbrot

--- src/MainScene.ts
+++ src/MainScene.ts
@@ -14,8 +14,8 @@ export default class MainScene extends Scene {
     material!: ShaderMaterial
 
     private readonly camera: OrthographicCamera
-
     private readonly clock: Clock = new Clock()
+    private readonly keys: Set<string> = new Set()
 
     constructor(camera: OrthographicCamera) {
         super()
@@ -23,8 +23,10 @@ export default class MainScene extends Scene {
         this.material = new ShaderMaterial({
             uniforms: {
                 time: { value: 0 },
-                resolution: { value: new Vector2() },
+                resolution: { value: new Vector2(0, 0) },
                 maxIterations: { value: 100 },
+                zoom: { value: 1.0 },
+                origin: { value: new Vector2(0, 0) },
             },
             fragmentShader: mandelbrotShader,
         })
@@ -32,16 +34,67 @@ export default class MainScene extends Scene {
     }
 
     init() {
+        // create plane
         const mesh = new Mesh(this.geometry, this.material)
         this.add(mesh)
 
-        //setup camera
+        // setup camera
         this.camera.position.z = 1
+
+        // setup inputs
+        document.addEventListener(
+            'keydown',
+            (event) => {
+                this.keys.add(event.key)
+            },
+            false,
+        )
+        document.addEventListener(
+            'keyup',
+            (event) => {
+                this.keys.delete(event.key)
+            },
+            false,
+        )
+    }
+
+    handleInputs() {
+        for (let key of this.keys) {
+            if (key === 'q') {
+                this.material.uniforms.maxIterations.value = Math.min(
+                    this.material.uniforms.maxIterations.value * 1.025,
+                    1000,
+                )
+            } else if (key === 'e') {
+                this.material.uniforms.maxIterations.value = Math.max(
+                    this.material.uniforms.maxIterations.value / 1.025,
+                    10,
+                )
+            } else if (key === 'z') {
+                this.material.uniforms.zoom.value *= 1.025
+            } else if (key === 'x') {
+                this.material.uniforms.zoom.value /= 1.025
+            } else if (key === 'w' || key === 'ArrowUp') {
+                this.material.uniforms.origin.value.y +=
+                    0.02 / this.material.uniforms.zoom.value
+            } else if (key === 'a' || key === 'ArrowLeft') {
+                this.material.uniforms.origin.value.x -=
+                    0.02 / this.material.uniforms.zoom.value
+            } else if (key === 's' || key === 'ArrowDown') {
+                this.material.uniforms.origin.value.y -=
+                    0.02 / this.material.uniforms.zoom.value
+            } else if (key === 'd' || key === 'ArrowRight') {
+                this.material.uniforms.origin.value.x +=
+                    0.02 / this.material.uniforms.zoom.value
+            }
+        }
     }
 
     update() {
         // const deltaTime = this.clock.getDelta()
         const elapsedTime = this.clock.getElapsedTime()
+
+        this.handleInputs()
 
         this.material.uniforms.time.value = elapsedTime
         this.material.uniforms.resolution.value.set(
--- src/shaders/mandelbrot.glsl
+++ src/shaders/mandelbrot.glsl
@@ -1,6 +1,17 @@
 uniform float time;
 uniform vec2 resolution;
 uniform int maxIterations;
+uniform float zoom;
+uniform vec2 origin;
+
+vec3 palette(float t) {
+    vec3 a = vec3(0.984, 0.250, 0.313);
+    vec3 b = vec3(0.923, 0.273, 0.054);
+    vec3 c = vec3(0.548, 0.807, 0.309);
+    vec3 d = vec3(2.598, 2.290, 4.940);
+
+    return a + b * cos(6.28318 * (c * t + d));
+}
 
 float mandelbrot(vec2 c) {
     vec2 z = vec2(0.0);
@@ -20,7 +31,10 @@ void main() {
     vec2 uv = gl_FragCoord.xy / resolution.xy * 2.0 - 1.0;
     uv.x *= resolution.x / resolution.y;
 
-    float i = mandelbrot(uv);
+    uv = uv / zoom + origin;
 
-    gl_FragColor = vec4(vec3(i), 1.0);
+    float i = mandelbrot(uv);
+    vec3 color = palette(i);
+
+    gl_FragColor = vec4(color, 1.0);
 }
